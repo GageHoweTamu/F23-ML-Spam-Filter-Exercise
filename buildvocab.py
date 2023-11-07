b99274d051dc9b9cad983ebcc5b522a78541e6f4
GageHoweGK <howe.gaged@gmail.com>
2023-11-06 20:50:31 -0600

    initial commit

--- buildvocab.py
+++ buildvocab.py
@@ -1,14 +1,20 @@
+# This generates vocabulary.txt, which is a list of words and the number of times they appear in spammy emails.
+# Top words included selectable, hypercube, toughest, critic, and desiring, with counts ranging from 37435 to 37440. That's insanely close.
+
 import pandas as pd
-
-data_sample = pd.read_csv("data\emails.csv")
-
+data_sample = pd.read_csv("data/emails.csv")
 vocabulary = {}
 
 # Build the vocabulary dictionary. If the word isn't in the vocabulary, add it
 def build_vocabulary(email_words):
-
+    for word in email_words:
+        if word not in vocabulary:
+            vocabulary[word] = len(vocabulary)
 
 # For every email in the dataset, split the email into individual words and call build_vocabulary
+for email in data_sample['text']:
+    email_words = email.split()
+    build_vocabulary(email_words)
 
 
 # Save the vocabulary to a file for further use
--- createfreqvec.py
+++ createfreqvec.py
@@ -1,3 +1,26 @@
+# This outputs an accuracy report for the trained model, such as:
+
+'''
+(5728, 37441)
+[[0. 0. 0. ... 0. 0. 0.]
+ [0. 0. 0. ... 0. 0. 0.]
+ [0. 0. 0. ... 0. 0. 0.]
+ ...
+ [0. 0. 0. ... 0. 0. 0.]
+ [0. 0. 0. ... 0. 0. 0.]
+ [0. 0. 0. ... 0. 0. 0.]]
+Accuracy: 0.9895287958115183
+Classification Report:               precision    recall  f1-score   support
+
+           0       1.00      0.99      0.99       856
+           1       0.97      0.99      0.98       290
+
+    accuracy                           0.99      1146
+   macro avg       0.98      0.99      0.99      1146
+weighted avg       0.99      0.99      0.99      1146
+
+'''
+
 import pandas as pd
 import numpy as np
 from sklearn.naive_bayes import MultinomialNB
@@ -6,13 +29,24 @@ from sklearn.metrics import accuracy_score, classification_report
 
 # Load vocabulary
 vocabulary = {}
-
+with open("vocabulary.txt", "r") as vocab_file:
+    for line in vocab_file:
+        word, index = line.strip().split("\t")
+        vocabulary[word] = int(index)
 
 # Load the dataset
-
+data_sample = pd.read_csv("data/emails.csv")
+X_sample = np.zeros((len(data_sample['text']), len(vocabulary))) # Create a matrix of zeros with the same number of rows as the dataset and the same number of columns as the vocabulary
+print(X_sample.shape)
+print(X_sample)
+y_sample = data_sample['spam']
 
 # Convert emails to frequency vectors
-
+for i, email in enumerate(data_sample['text']):
+    email_words = email.split()
+    for word in email_words:
+        if word in vocabulary:
+            X_sample[i, vocabulary.get(word.lower(), 0)] += 1 # Increment the count for this word in email 
 
 # Split data
 X_train, X_test, y_train, y_test = train_test_split(X_sample, y_sample, test_size=0.2, random_state=42)
@@ -31,7 +65,13 @@ train_test_split outputs these:
 
 
 # Train Naive Bayes
-
+clf = MultinomialNB()
+clf.fit(X_train, y_train)
 
 # Predict and evaluate
+yy_pred = clf.predict(X_test)
+accuracy = accuracy_score(y_test, yy_pred)
+classification_rep = classification_report(y_test, yy_pred)
 
+print(f"Accuracy: {accuracy}")
+print("Classification Report:", classification_rep)
